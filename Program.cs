82e00b1d477b287fc086049b17b885e614b27302
isabellagoes <isabellagoes.igs@gmail.com>
2023-11-26 23:34:48 -0300

    'Eu sou Groot' disse Groot

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿const string FraseSaida = "tchau";
+
+Console.WriteLine("----- Groot Chatbot -----\n");
+
+string Você;
+do
+{
+    Console.Write("Você: ");
+    Você = Console.ReadLine()!;
+
+    string Groot = ObtemRespostaGroot(Você);
+
+    ExibeResposta(ObtemRespostaGroot(Você));
+
+}
+
+while (Você.Trim().ToLower() != FraseSaida);
+
+void ExibeResposta(string Groot)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"                          Groot: {Groot}");
+    Console.ResetColor();
+}
+
+string ObtemRespostaGroot(string Você)
+{
+    return Você.Trim().ToLower() == FraseSaida ?
+        "Eu sou Groot!" :
+        "Eu sou Groot.";
+}
